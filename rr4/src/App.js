3d038c340a7012ff1d59331fdc1f0a97306112fb
Oren Kredo <okredo@yahoo.com>
2017-03-20 19:56:51 -0700

    lesson three using the v4 Link component

--- rr4/src/App.js
+++ rr4/src/App.js
@@ -1,6 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-//console.log(BrowserRouter, 'BrowserRouter')
+import { BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+import './App.css';
+
+const Links = () => (
+  <nav>
+	<Link to="/">Home</Link>
+	<Link to={{ pathname: '/about'}}>About</Link>
+	<Link replace to="/contact">Contact</Link>
+  </nav>
+)
 
 const Home = (props) => {
 	console.log(props, 'props');
@@ -11,9 +23,12 @@ const App = () => (
 
   <Router>
 	<div>
+	  <Links />
 	  <Route exact path="/" component={Home} />
 	  <Route path="/about"
-			 children={({match})=> match && <h1>About</h1>}/>
+			 children={({match})=> match && <h1>About</h1>} />
+	  <Route path="/contact" render={()=> <h1>Contact</h1>} />
+
 	</div>
   </Router>
 );
